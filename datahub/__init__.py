cf6be5d646f0859783fe4de0fc4d3f425a0cbba5
Taehyon Lee <leeth7830@gmail.com>
2018-11-23 22:30:07 -0800

    blueprint

--- datahub/__init__.py
+++ datahub/__init__.py
@@ -2,14 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from datahub.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'd01a2fef62338c99179cb2d1752c1a16'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from datahub import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from datahub.users.routes import users
+    from datahub.posts.routes import posts
+    from datahub.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from datahub import app
+from datahub import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
