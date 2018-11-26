e0d3ef6734177fa055385ddbe487e2643c82a6f3
Taehyon Lee <leeth7830@gmail.com>
2018-11-25 20:41:10 -0800

    connection to VPS

--- datahub/__init__.py
+++ datahub/__init__.py
@@ -26,8 +26,10 @@ def create_app(config_class=Config):
     from datahub.users.routes import users
     from datahub.posts.routes import posts
     from datahub.main.routes import main
+    from datahub.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
