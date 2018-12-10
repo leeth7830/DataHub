ef9cf60a2acf3f8cee0142bc084fd0f49e0949e1
Taehyon Lee <leeth7830@gmail.com>
2018-12-09 21:29:15 -0800

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from datahub import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='23.226.138.66', debug=True)
+    app.run(host='23.226.138.66', port=80, debug=True)
